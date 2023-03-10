2a0b0b1bfab0bb9220d7adc88df98818f8df7611
devansh-khandelwal <devanshk2107@gmail.com>
2023-03-11 00:20:34 +0530

    update sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import "./App.css";
+import "./styles/App.css";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
+import logo from "./images/logo3.png";
 
 function App() {
-    // const { collapseSidebar } = useProSidebar();
     return (
-        <div className="sidebarContainer">
-            <Sidebar
-                rootStyles={{
-                    backgroundColor: "#D5DCF0",
-                    height: "96vh",
-                }}
-            >
-                <Menu>
-                    <MenuItem> Score Card </MenuItem>
-                    <MenuItem> Risk Management </MenuItem>
-                    <MenuItem> Prior Authorization </MenuItem>
-                    <MenuItem> Claims </MenuItem>
-                    <MenuItem> My Profile </MenuItem>
-                </Menu>
-            </Sidebar>
+        <div className="container">
+            <div className="logoConatiner">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="sidebarContainer">
+                <Sidebar
+                    rootStyles={{
+                        backgroundColor: "white",
+                        height: "86.5vh",
+                    }}
+                >
+                    <Menu>
+                        <MenuItem> Score Card </MenuItem>
+                        <MenuItem> Risk Management </MenuItem>
+                        <MenuItem> Prior Authorization </MenuItem>
+                        <MenuItem> Claims </MenuItem>
+                        <MenuItem> My Profile </MenuItem>
+                    </Menu>
+                </Sidebar>
+            </div>
         </div>
     );
 }
